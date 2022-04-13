3a99f792159dde95f6b6de0331150e081e380c39
YanTaiXianSheng <YanTaiXianSheng@qq.com>
2022-04-13 11:13:10 +0800

    Third commit

--- src/main/java/com/jxcia/git/GitTest.java
+++ src/main/java/com/jxcia/git/GitTest.java
@@ -3,5 +3,6 @@ package com.jxcia.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("This is my Object , One");
+        System.out.println("This is my Object , Two");
     }
 }
